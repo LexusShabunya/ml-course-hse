5c114eb4c576fdfa4a73b53c1cf1047054faced4
LexusShabunya <alekseyshabunya@gmail.com>
2023-12-09 23:57:44 +0300

    Done HW5

--- 2023-fall/homeworks-practice/homework-practice-05-trees/hw5code.py
+++ 2023-fall/homeworks-practice/homework-practice-05-trees/hw5code.py
@@ -25,13 +25,40 @@ def find_best_split(feature_vector, target_vector):
     :return threshold_best: оптимальный порог (число)
     :return gini_best: оптимальное значение критерия Джини (число)
     """
-    # ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ
+    n = len(feature_vector)
 
-    pass
+    sorted_indices = np.argsort(feature_vector)
+    sorted_features = feature_vector[sorted_indices]
+    sorted_targets = target_vector[sorted_indices]
+
+    left_node_sizes = np.arange(1, n)
+    right_node_sizes = n - left_node_sizes
+
+    pos_cnt = np.sum(target_vector)
+    left_pos_cnts = np.cumsum(sorted_targets[:-1])
+    right_pos_cnts = pos_cnt - left_pos_cnts
+
+    left_pos_probs = left_pos_cnts / left_node_sizes
+    right_pos_probs = right_pos_cnts / right_node_sizes
+
+    left_impurties = 1 - left_pos_probs**2 - (1 - left_pos_probs)**2
+    right_impurties = 1 - right_pos_probs**2 - (1 - right_pos_probs)**2
+
+    different_elements = sorted_features[:-1] != sorted_features[1:]
+
+    thresholds = (sorted_features[:-1] + sorted_features[1:]) / 2
+    ginis = - left_node_sizes / n * left_impurties - right_node_sizes / n * right_impurties
+
+    thresholds = thresholds[different_elements]
+    ginis = ginis[different_elements]
+
+    best_gini_index = np.argmax(ginis)
+
+    return thresholds, ginis, thresholds[best_gini_index], ginis[best_gini_index]
 
 
-class DecisionTree:
-    def __init__(self, feature_types, max_depth=None, min_samples_split=None, min_samples_leaf=None):
+class DecisionTree():
+    def __init__(self, feature_types, max_depth=None, min_samples_split=None, min_samples_leaf=1): 
         if np.any(list(map(lambda x: x != "real" and x != "categorical", feature_types))):
             raise ValueError("There is unknown feature type")
 
@@ -42,13 +69,26 @@ class DecisionTree:
         self._min_samples_leaf = min_samples_leaf
 
     def _fit_node(self, sub_X, sub_y, node):
-        if np.all(sub_y != sub_y[0]):
+        if len(self._tree) == 0:
+            node["layer"] = 1
+        
+        if (not (self._max_depth is None)) and node["layer"] + 1 > self._max_depth:
+            node["type"] = "terminal"
+            node["class"] = Counter(sub_y).most_common(1)[0][0]
+            return
+        
+        if (not (self._min_samples_split is None)) and len(sub_y) < self._min_samples_split:
+            node["type"] = "terminal"
+            node["class"] = Counter(sub_y).most_common(1)[0][0]
+            return
+
+        if np.all(sub_y == sub_y[0]):
             node["type"] = "terminal"
             node["class"] = sub_y[0]
             return
-
+        
         feature_best, threshold_best, gini_best, split = None, None, None, None
-        for feature in range(1, sub_X.shape[1]):
+        for feature in range(sub_X.shape[1]):
             feature_type = self._feature_types[feature]
             categories_map = {}
 
@@ -58,39 +98,41 @@ class DecisionTree:
                 counts = Counter(sub_X[:, feature])
                 clicks = Counter(sub_X[sub_y == 1, feature])
                 ratio = {}
+
                 for key, current_count in counts.items():
                     if key in clicks:
                         current_click = clicks[key]
                     else:
                         current_click = 0
-                    ratio[key] = current_count / current_click
-                sorted_categories = list(map(lambda x: x[1], sorted(ratio.items(), key=lambda x: x[1])))
-                categories_map = dict(zip(sorted_categories, list(range(len(sorted_categories)))))
+                    
+                    ratio[key] = current_click / current_count 
 
-                feature_vector = np.array(map(lambda x: categories_map[x], sub_X[:, feature]))
+                sorted_categories = list(map(lambda x: x[0], sorted(ratio.items(), key=lambda x: x[1])))
+                categories_map = dict(zip(sorted_categories, list(range(len(sorted_categories)))))
+                feature_vector = np.array(list(map(lambda x: categories_map[x], sub_X[:, feature])))
             else:
                 raise ValueError
 
-            if len(feature_vector) == 3:
+            if np.all(feature_vector == feature_vector[0]):  
                 continue
 
             _, _, threshold, gini = find_best_split(feature_vector, sub_y)
-            if gini_best is None or gini > gini_best:
+            if (gini_best is None or gini > gini_best) and gini is not None:
                 feature_best = feature
                 gini_best = gini
                 split = feature_vector < threshold
-
+                
                 if feature_type == "real":
                     threshold_best = threshold
-                elif feature_type == "Categorical":
-                    threshold_best = list(map(lambda x: x[0],
-                                              filter(lambda x: x[1] < threshold, categories_map.items())))
+                elif feature_type == "categorical":
+                    threshold_best = set((map(lambda x: x[0],
+                                              filter(lambda x: x[1] < threshold, categories_map.items()))))
                 else:
                     raise ValueError
 
         if feature_best is None:
             node["type"] = "terminal"
-            node["class"] = Counter(sub_y).most_common(1)
+            node["class"] = Counter(sub_y).most_common(1)[0][0]
             return
 
         node["type"] = "nonterminal"
@@ -102,13 +144,33 @@ class DecisionTree:
             node["categories_split"] = threshold_best
         else:
             raise ValueError
-        node["left_child"], node["right_child"] = {}, {}
+        
+        if len(sub_y[split]) < self._min_samples_leaf or len(sub_y[np.logical_not(split)]) < self._min_samples_leaf:
+            node["type"] = "terminal"
+            node["class"] = Counter(sub_y).most_common(1)[0][0]
+            return
+        
+        node["left_child"], node["right_child"] = {"layer": node["layer"]+1}, {"layer": node["layer"]+1}
+
         self._fit_node(sub_X[split], sub_y[split], node["left_child"])
-        self._fit_node(sub_X[np.logical_not(split)], sub_y[split], node["right_child"])
+        self._fit_node(sub_X[np.logical_not(split)], sub_y[np.logical_not(split)], node["right_child"])
 
     def _predict_node(self, x, node):
-        # ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ
-        pass
+        if node["type"] == "terminal":
+            return node["class"]
+        
+        feature_split = node["feature_split"]
+        
+        if self._feature_types[feature_split] == "real":
+            if x[feature_split] < node["threshold"]:
+                return self._predict_node(x, node["left_child"])
+            
+            return self._predict_node(x, node["right_child"])
+        elif self._feature_types[feature_split] == "categorical":
+            if x[feature_split] in node["categories_split"]:
+                return self._predict_node(x, node["left_child"])
+
+            return self._predict_node(x, node["right_child"])
 
     def fit(self, X, y):
         self._fit_node(X, y, self._tree)
@@ -118,3 +180,10 @@ class DecisionTree:
         for x in X:
             predicted.append(self._predict_node(x, self._tree))
         return np.array(predicted)
+    
+    def get_params(self, deep=True):
+        return {"feature_types": self._feature_types, 
+                "max_depth": self._max_depth, 
+                "min_samples_split": self._min_samples_split, 
+                "min_samples_leaf": self._min_samples_leaf}
+
